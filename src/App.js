42940dba2266b2455d2a048cb7fbf832b3650157
MohammedAlhawamdeh <hawamdeh9393@gmail.com>
2021-03-04 10:58:06 +0000

    from other

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
+import LoginProvider from './Components/auth/context'
+import Signup from './Components/auth/signup'
 import ToDo from './Components/todo-connected'
 
 export default function App() {
   return (
     <>
-      <ToDo />
+      <LoginProvider>
+        <ToDo />
+        <Signup />
+      </LoginProvider>
     </>
   )
 }
---
+++ src/Components/auth/context.js
@@ -0,0 +1,92 @@
+import React, { useState, useCallback, useEffect } from 'react'
+import axios from 'axios'
+import cookie from 'react-cookies';
+import jwt from 'jsonwebtoken';
+
+
+export const LoginContext = React.createContext()
+
+const API = 'https://api-js401.herokuapp.com'
+
+const LoginProvider = (props) => {
+
+    const [loggedIn, setLoggedIn] = useState(false)
+    const [user, setUser] = useState({})
+    const [token, setToken] = useState()
+    const can = (capability) => {
+        return state.user?.capabilities?.includes(capability);
+    }
+
+    const login = (username, password) => {
+        const auth = { username, password };
+        axios.post(`${API}/signin`, {}, { auth })
+            .then(response => {
+                validateToken(response?.data?.token);
+            })
+            .catch(console.error);
+    }
+
+    const signup = (state) => {
+        axios({
+            method: 'post',
+            url: `${API}/signup`,
+            data: state,
+
+        })
+            .then(response => {
+                validateToken(response?.data?.token);
+            })
+            .catch(console.error);
+    }
+
+    const validateToken = useCallback(token => {
+        try {
+            const user = jwt.verify(token, "supersecret");
+            setLoginState(true, token, user);
+        }
+        catch (e) {
+            console.log('in validate token');
+            setLoginState(false, null, {});
+            console.log('Token Validation Error', e);
+        }
+
+    }, []);
+
+    const logout = () => {
+        setLoginState(false, null, {});
+    };
+
+    const setLoginState = async (loggedIn, token, user) => {
+        cookie.save('auth', token);
+        await setToken(token);
+        await setLoggedIn(loggedIn);
+        await setUser(user)
+    };
+
+    useEffect(() => {
+        const qs = new URLSearchParams(window.location.search);
+        const cookieToken = cookie.load('auth');
+        const token = qs.get('token') || cookieToken || null;
+        validateToken(token);
+    }, [validateToken]);
+
+    const state = {
+        loggedIn,
+        login,
+        signup,
+        logout,
+        can: can,
+        user,
+        token,
+    };
+
+
+    return (
+        <LoginContext.Provider value={state}>
+            {props.children}
+        </LoginContext.Provider>
+    );
+
+}
+
+export default LoginProvider;
